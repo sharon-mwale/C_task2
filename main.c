4ea8790b0cd520473f1fbee95b688931ad640db8
sharon mwale <sharoonmwale@gmail.com>
2022-10-12 02:55:39 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+typedef struct {
+    char* name;
+    int number;
+}person
+
+int main (int agrc, argv[]){
+    person pople[2];
+    people[0].name ="neria";
+    people[0].number = 123456;
+
+    people[1].name ="neria";
+    people[1].number = 678904;
+
+    for(size_t i= 0; i< 2; i++)
+    {
+
+    };
+
+    return 0;
+}
---
+++ math.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+#include <math.h>
+int main(){
+    
+    int a,b,c;
+    printf("Enter your a, b, b and c value./n");
+    scanf(%d %d d%, &a, &b, &c)
+    
+    int power = pow(b, 2);// b**2 b^2
+    int product = 4 * a * c;//4ac
+    int sub = power - product;// b**2 - 4ac
+
+    double sqaure_root = sqrt(sub); //vb** 2 - 4ac
+    double add = (-b + square_root) / (2 * a);
+    double sub = (-b + square_root)
+
+    return 0;
+}
